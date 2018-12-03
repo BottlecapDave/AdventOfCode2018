2dd5d6d67463e4f00b1efd8266bc153bf5226b09
David Kendall <david@rocketmakers.com>
2018-12-03 13:34:05 +0000

    Added solution for day 1

---
+++ src/AdventOfCode2018/AdventOfCode2018/Day01.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2018
+{
+    public class Day01
+    {
+        public int PartOne(string filePath)
+        {
+            var values = File.ReadLines(filePath).Select(x => Int32.Parse(x)).ToArray();
+            return PartOne(values);
+        }
+
+        public int PartOne(params int[] numbers)
+        {
+            return numbers.Aggregate((current, next) => current + next);
+        }
+
+        public int PartTwo(string filePath)
+        {
+            var values = File.ReadLines(filePath).Select(x => Int32.Parse(x)).ToArray();
+            return PartTwo(values);
+        }
+
+        public int PartTwo(params int[] numbers)
+        {
+            var discoveredValues = new HashSet<int>();
+            var currentValue = 0;
+            discoveredValues.Add(currentValue);
+
+            for (; ; )
+            {
+                foreach (var number in numbers)
+                {
+                    currentValue += number;
+
+                    if (discoveredValues.Contains(currentValue))
+                    {
+                        return currentValue;
+                    }
+
+                    discoveredValues.Add(currentValue);
+                }
+            }
+        }
+    }
+}
---
+++ src/AdventOfCode2018/UnitTests.AdventOfCode2018/Day01Tests.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using AdventOfCode2018;
+using Xunit;
+
+namespace UnitTests.AdventOfCode2018
+{
+    public class Day01Tests
+    {
+        [Fact]
+        public void PartOne_When_ExampleOne_Then_ResultIsThree()
+        {
+            // Arrange
+            var eval = new Day01();
+
+            // Act
+            var result = eval.PartOne(1, 1, 1);
+
+            // Assert
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void PartOne_When_ExampleTwo_Then_ResultIsZero()
+        {
+            // Arrange
+            var eval = new Day01();
+
+            // Act
+            var result = eval.PartOne(1, 1, -2);
+
+            // Assert
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void PartOne_When_ExampleThree_Then_ResultIsMinusSix()
+        {
+            // Arrange
+            var eval = new Day01();
+
+            // Act
+            var result = eval.PartOne(-1, -2, -3);
+
+            // Assert
+            Assert.Equal(-6, result);
+        }
+
+        [Fact]
+        public void PartOne_When_ExampleFile_Then_ResultIsAnswer()
+        {
+            // Arrange
+            var eval = new Day01();
+
+            // Act
+            var result = eval.PartOne("TestData/01.txt");
+
+            // Assert
+            Assert.Equal(486, result);
+        }
+
+        [Fact]
+        public void PartTwo_When_ExampleOne_Then_ResultIsZero()
+        {
+            // Arrange
+            var eval = new Day01();
+
+            // Act
+            var result = eval.PartTwo(1, -1);
+
+            // Assert
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void PartTwo_When_ExampleTwo_Then_ResultIsTen()
+        {
+            // Arrange
+            var eval = new Day01();
+
+            // Act
+            var result = eval.PartTwo(3, 3, 4, -2, -4);
+
+            // Assert
+            Assert.Equal(10, result);
+        }
+
+        [Fact]
+        public void PartTwo_When_ExampleThree_Then_ResultIsFive()
+        {
+            // Arrange
+            var eval = new Day01();
+
+            // Act
+            var result = eval.PartTwo(-6, 3, 8, 5, -6);
+
+            // Assert
+            Assert.Equal(5, result);
+        }
+
+        [Fact]
+        public void PartTwo_When_ExampleThree_Then_ResultIsFourteen()
+        {
+            // Arrange
+            var eval = new Day01();
+
+            // Act
+            var result = eval.PartTwo(7, 7, -2, -7, -4);
+
+            // Assert
+            Assert.Equal(14, result);
+        }
+
+        [Fact]
+        public void PartTwo_When_ExampleFile_Then_ResultIsAnswer()
+        {
+            // Arrange
+            var eval = new Day01();
+
+            // Act
+            var result = eval.PartTwo("TestData/01.txt");
+
+            // Assert
+            Assert.Equal(69285, result);
+        }
+    }
+}
