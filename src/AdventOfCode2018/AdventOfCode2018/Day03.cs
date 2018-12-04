11d5360c618632f550f0db87d4419bbdfd16c946
David Kendall <me@davidskendall.co.uk>
2018-12-04 13:21:36 +0000

    Added solution for part 1 of day 3

---
+++ src/AdventOfCode2018/AdventOfCode2018/Day03.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2018
+{
+    public class Day03
+    {
+        public readonly Regex _regex = new Regex("^#(?<id>\\d+) @ (?<x>\\d+),(?<y>\\d+): (?<width>\\d+)x(?<height>\\d+)$");
+
+        public int PartOne(string filePath)
+        {
+            var values = File.ReadLines(filePath).ToArray();
+            return PartOne(values);
+        }
+
+        public int PartOne(params string[] values)
+        {
+            var gridContent = new Dictionary<KeyValuePair<long, long>, List<long>>();
+            var overlappedAreas = new List<KeyValuePair<long, long>>();
+
+            foreach (var value in values)
+            {
+                var match = _regex.Match(value);
+                if (match.Success == false)
+                {
+                    throw new InvalidDataException($"Unexpected format for '{value}' encountered.");
+                }
+
+                var id = Int64.Parse(match.Groups["id"].Value);
+                var x = Int64.Parse(match.Groups["x"].Value);
+                var y = Int64.Parse(match.Groups["y"].Value);
+                var width = Int64.Parse(match.Groups["width"].Value);
+                var height = Int64.Parse(match.Groups["height"].Value);
+
+                for (int i = 0; i < width; i++)
+                {
+                    for (int j = 0; j < height; j++)
+                    {
+                        var space = new KeyValuePair<long, long>(x + i, y + j);
+
+                        // retrieve our target space and add our id to the collection of patches that have been assigned that space.
+                        if (gridContent.TryGetValue(space, out List<long> ids) == false)
+                        {
+                            ids = new List<long>();
+                            gridContent.Add(space, ids);
+                        }
+
+                        ids.Add(id);
+
+                        // If we have at least 2 items on our current space, then add the space to our list of overlapped areas
+                        if (ids.Count == 2)
+                        {
+                            overlappedAreas.Add(space);
+                        }
+                    }
+                }
+            }
+
+            return overlappedAreas.Count;
+        }
+    }
+}
---
+++ src/AdventOfCode2018/UnitTests.AdventOfCode2018/Day03Tests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using AdventOfCode2018;
+using Xunit;
+
+namespace UnitTests.AdventOfCode2018
+{
+    public class Day03Tests
+    {
+        [Fact]
+        public void PartOne_When_ExampleOne_Then_ResultIsTwelve()
+        {
+            // Arrange
+            var eval = new Day03();
+
+            // Act
+            var result = eval.PartOne("#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2");
+
+            // Assert
+            Assert.Equal(4, result);
+        }
+
+        [Fact]
+        public void PartOne_When_ExampleFile_Then_ResultIsAnswer()
+        {
+            // Arrange
+            var eval = new Day03();
+
+            // Act
+            var result = eval.PartOne("TestData/03.txt");
+
+            // Assert
+            Assert.Equal(109716, result);
+        }
+    }
+}
