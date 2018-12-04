69d348a14e795d46f27586a8e87c5cdf9a7a3d8e
David Kendall <me@davidskendall.co.uk>
2018-12-04 17:37:24 +0000

    Started a solution for part 1 of day 4. Need to do the last bit

---
+++ src/AdventOfCode2018/AdventOfCode2018/Day04.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2018
+{
+    public class Day04
+    {
+        private class ShiftEvent
+        {
+            public DateTime Timestamp { get; set; }
+
+            public EventType Type { get; set; }
+
+            public long? GuardId { get;  set;}
+        }
+
+        private class SleepingPattern
+        {
+            public long GuardId { get; set; }
+
+            public long[] TimesPerMinute { get; set; }
+
+            public long TotalMinutes { get; set; }
+
+            public SleepingPattern(long guardId)
+            {
+                GuardId = guardId;
+                TimesPerMinute = new long[60];
+            }
+        }
+
+        private enum EventType
+        {
+            ShiftStart,
+            FellAsleep,
+            WokeUp
+        }
+
+        public readonly Regex _beginShiftRegex = new Regex("^\\[(?<time>[^\\]]+)\\] Guard #(?<id>\\d+) begins shift$");
+        public readonly Regex _fallsAsleepShiftRegex = new Regex("^\\[(?<time>[^\\]]+)\\] falls asleep$");
+        public readonly Regex _wakesUpShiftRegex = new Regex("^\\[(?<time>[^\\]]+)\\] wakes up$");
+
+        public int PartOne(string filePath)
+        {
+            var values = File.ReadLines(filePath).ToArray();
+            return PartOne(values);
+        }
+
+        public int PartOne(params string[] values)
+        {
+            var events = ProcessEvents(values);
+
+            var guardAsleepDictionary = new Dictionary<long, SleepingPattern>();
+            long? currentGuardId = null;
+            DateTime? currentFellAsleep = null;
+
+            foreach (var shiftEvent in events)
+            {
+                switch (shiftEvent.Type)
+                {
+                    case EventType.ShiftStart:
+                        currentGuardId = shiftEvent.GuardId;
+                        break;
+                    case EventType.FellAsleep:
+                        currentFellAsleep = shiftEvent.Timestamp;
+                        break;
+                    case EventType.WokeUp:
+
+                        if (guardAsleepDictionary.TryGetValue(currentGuardId.Value, out SleepingPattern pattern) == false)
+                        {
+                            pattern = new SleepingPattern(currentGuardId.Value);
+                            guardAsleepDictionary.Add(currentGuardId.Value, pattern);
+                        }
+
+                        var totalMinutes = 0;
+                        while (currentFellAsleep < shiftEvent.Timestamp)
+                        {
+                            pattern.TimesPerMinute[currentFellAsleep.Value.Minute]++;
+                            currentFellAsleep.Value.AddMinutes(1);
+                            totalMinutes++;
+                        }
+
+                        pattern.TotalMinutes += totalMinutes;
+                        break;
+                }
+
+            }
+
+            var targetGuard = guardAsleepDictionary.Values.OrderBy(x => x.TotalMinutes).FirstOrDefault();
+        }
+
+        private IEnumerable<ShiftEvent> ProcessEvents(string[] values)
+        {
+            var events = new List<ShiftEvent>();
+
+            foreach (var value in values)
+            {
+                var match = _beginShiftRegex.Match(value);
+                if (match.Success)
+                {
+                    var timestamp = DateTime.Parse(match.Groups["time"].Value);
+                    var guardId = Int64.Parse(match.Groups["id"].Value);
+                    events.Add(new ShiftEvent()
+                    {
+                        GuardId = guardId,
+                        Timestamp = timestamp,
+                        Type = EventType.ShiftStart
+                    });
+
+                    continue;
+                }
+
+                match = _fallsAsleepShiftRegex.Match(value);
+                if (match.Success)
+                {
+                    var timestamp = DateTime.Parse(match.Groups["time"].Value);
+                    events.Add(new ShiftEvent()
+                    {
+                        Timestamp = timestamp,
+                        Type = EventType.FellAsleep
+                    });
+
+                    continue;
+                }
+
+                match = _wakesUpShiftRegex.Match(value);
+                if (match.Success)
+                {
+                    var timestamp = DateTime.Parse(match.Groups["time"].Value);
+                    events.Add(new ShiftEvent()
+                    {
+                        Timestamp = timestamp,
+                        Type = EventType.WokeUp
+                    });
+
+                    continue;
+                }
+
+            }
+
+            events.Sort((first, second) => first.Timestamp.CompareTo(second.Timestamp));
+
+            return events;
+        }
+    }
+}
---
+++ src/AdventOfCode2018/UnitTests.AdventOfCode2018/Day04Tests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using AdventOfCode2018;
+using Xunit;
+
+namespace UnitTests.AdventOfCode2018
+{
+    public class Day04Tests
+    {
+        [Fact]
+        public void PartOne_When_ExampleOne_Then_ResultIsFour()
+        {
+            // Arrange
+            var eval = new Day04();
+
+            // Act
+            var result = eval.PartOne("TestData/04.example.txt");
+
+            // Assert
+            Assert.Equal(240, result);
+        }
+
+        [Fact]
+        public void PartOne_When_ExampleFile_Then_ResultIsAnswer()
+        {
+            // Arrange
+            var eval = new Day04();
+
+            // Act
+            var result = eval.PartOne("TestData/04.txt");
+
+            // Assert
+            Assert.Equal(109716, result);
+        }
+    }
+}
