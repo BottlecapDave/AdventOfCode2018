2c89e5645da776a08057ec787db832511af56078
David Kendall <me@davidskendall.co.uk>
2018-12-05 08:28:38 +0000

    Implemented part 1 of day 5

---
+++ src/AdventOfCode2018/AdventOfCode2018/Day05.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCode2018
+{
+    public class Day05
+    {
+        public long PartOneWithFile(string filePath)
+        {
+            var value = File.ReadAllText(filePath);
+            return PartOne(value.Trim());
+        }
+
+        public long PartOne(string value)
+        {
+            var currentCharacters = new Stack<char>();
+            foreach (var character in value)
+            {
+                var lastCharacter = currentCharacters.Count > 0 ? (char?)currentCharacters.Peek() : null;
+                if (lastCharacter.HasValue &&
+                    Char.ToUpper(character) == Char.ToUpper(lastCharacter.Value) &&
+                    ((Char.IsUpper(character) && Char.IsLower(lastCharacter.Value)) ||
+                     (Char.IsLower(character) && Char.IsUpper(lastCharacter.Value))))
+                {
+                    currentCharacters.Pop();
+                }
+                else
+                {
+                    currentCharacters.Push(character);
+                }
+            }
+
+            return currentCharacters.Count;
+        }
+    }
+}
---
+++ src/AdventOfCode2018/UnitTests.AdventOfCode2018/Day05Tests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using AdventOfCode2018;
+using Xunit;
+
+namespace UnitTests.AdventOfCode2018
+{
+    public class Day05Tests
+    {
+        [Fact]
+        public void PartOne_When_ExampleOne_Then_ResultIsExampleAnswer()
+        {
+            // Arrange
+            var eval = new Day05();
+
+            // Act
+            var result = eval.PartOne("dabAcCaCBAcCcaDA");
+
+            // Assert
+            Assert.Equal(10, result);
+        }
+
+        [Fact]
+        public void PartOne_When_ExampleFile_Then_ResultIsAnswer()
+        {
+            // Arrange
+            var eval = new Day05();
+
+            // Act
+            var result = eval.PartOneWithFile("TestData/05.txt");
+
+            // Assert
+            Assert.Equal(9371, result);
+        }
+    }
+}
